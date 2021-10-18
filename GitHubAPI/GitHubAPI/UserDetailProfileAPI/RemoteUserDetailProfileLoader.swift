11d56c707603b1ef2c631a8c2b49dfe0a2be8023
ctwdtw <ctwdtw@gmail.com>
2021-10-18 16:55:22 +0800

    duplicate `RemoteUserProfileLoader` as `RemoteUserDetailProfileLoader`

---
+++ GitHubAPI/GitHubAPI/UserDetailProfileAPI/RemoteUserDetailProfileLoader.swift
@@ -0,0 +1,50 @@
+//
+//  RemoteUserDetailProfileLoader.swift
+//  GitHubAPI
+//
+//  Created by Paul Lee on 2021/10/18.
+//
+
+import Foundation
+import Alamofire
+
+public class RemoteUserDetailProfileLoader {
+    struct RemoteUserProfile: Codable {
+        let id: Int
+        let login: String
+        let avatar_url: URL
+        let site_admin: Bool
+    }
+    
+    let url: URL
+    
+    let session: Session
+    
+    let mapper: UserDetailProfileMapper
+    
+    public init(url: URL, session: Session = .default, mapper: UserDetailProfileMapper) {
+        self.url = url
+        self.session = session
+        self.mapper = mapper
+    }
+    
+    public func load(complete: @escaping LoadUserProfileComplete) {
+        session.request(url).validate(statusCode: mapper.validStatusCodes).responseDecodable(of: [RemoteUserProfile].self) {  [weak self] response in
+            
+            guard let self = self else {
+                complete(.failure(UserProfileMapper.Error.loaderHasDeallocated))
+                return
+            }
+            
+            do {
+                let profiles = try self.mapper.map(response)
+                complete(.success(profiles))
+                
+            } catch {
+                complete(.failure(error))
+                
+            }
+
+        }
+    }
+}
---
+++ GitHubAPI/GitHubAPI/UserDetailProfileAPI/UserDetailProfileMapper.swift
@@ -0,0 +1,75 @@
+//
+//  UserDetailProfileMapper.swift
+//  GitHubAPI
+//
+//  Created by Paul Lee on 2021/10/18.
+//
+
+import Foundation
+import Alamofire
+
+public class UserDetailProfileMapper {
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+        case notModified
+        case loaderHasDeallocated
+        case unexpected
+    }
+    
+    var validStatusCodes: [Int] {
+        return [200]
+    }
+    
+    var nonModifiedStatusCode: Int {
+        304
+    }
+    
+    private(set) var currentHeaders: [AnyHashable: Any]?
+    
+    public init() {}
+    
+    func map(_ response: DataResponse<[RemoteUserDetailProfileLoader.RemoteUserProfile], AFError>) throws -> [UserProfile] {
+        currentHeaders = response.response?.allHeaderFields
+        
+        if let remoteProfiles = response.value {
+            let profiles = remoteProfiles.map { UserProfile(id: $0.id, login: $0.login, avatarUrl: $0.avatar_url, siteAdmin: $0.site_admin) }
+            return profiles
+            
+        } else if response.response?.statusCode == self.nonModifiedStatusCode {
+            throw UserProfileMapper.Error.notModified
+            
+        } else if let error = response.error {
+            if error.isSessionTaskError {
+                throw UserProfileMapper.Error.connectivity
+                
+            } else {
+                throw UserProfileMapper.Error.invalidData
+                
+            }
+            
+        } else {
+            throw UserProfileMapper.Error.unexpected
+            
+        }
+    }
+    
+    //MARK: - paginated api detail
+    private let linkKey = "Link"
+    
+    func nextURL() -> URL? {
+        guard let nextLink = (currentHeaders?[linkKey] as? String)?.split(separator: ",").filter({ $0.contains("next") }).first else {
+            return nil
+        }
+        
+        guard let range = nextLink.range(of: "(?<=\\<).+?(?=\\>)", options: .regularExpression) else {
+            return nil
+        }
+        
+        guard let url = URL(string: String(nextLink[range])) else {
+            return nil
+        }
+        
+        return url
+    }
+}
