d2f765170672008c69b069a7678757f4a4b862fa
ctwdtw <ctwdtw@gmail.com>
2021-10-26 14:13:35 +0800

    move `RemoteImageDataLoader` to prod. code

---
+++ GitHubAPI/GitHubAPI/UserProfileAPI/RemoteImageDataLoader.swift
@@ -0,0 +1,82 @@
+//
+//  RemoteImageDataLoader.swift
+//  GitHubAPI
+//
+//  Created by Paul Lee on 2021/10/26.
+//
+
+import Foundation
+import Alamofire
+
+public protocol ImageDataTask {
+    func cancel()
+}
+
+public class RemoteImageDataLoader {
+    class RemoteImageDataTask: ImageDataTask {
+        var request: DataRequest?
+        private(set) var completion: Complete?
+        
+        init(complete: @escaping Complete) {
+            self.completion = complete
+        }
+        
+        func cancel() {
+            request?.cancel()
+            completion = nil
+        }
+        
+        func complete(with result: Result) {
+            completion?(result)
+        }
+        
+    }
+    
+    public typealias Result = Swift.Result<Data, Error>
+    
+    public typealias Complete = (Result) -> Void
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    private let session: Session
+    
+    public init(session: Session) {
+        self.session = session
+    }
+    
+    @discardableResult
+    public func load(url: URL, complete: @escaping Complete) -> ImageDataTask {
+        let task = RemoteImageDataTask(complete: complete)
+        
+        let request = session.request(url).responseData { [weak self] response in
+            guard self != nil else {
+                return
+            }
+            
+            guard let statusCode = response.response?.statusCode else {
+                task.complete(with: .failure(.connectivity))
+                return
+            }
+            
+            guard statusCode == 200 else {
+                task.complete(with: .failure(.invalidData))
+                return
+            }
+            
+            guard let data = response.data else {
+                task.complete(with: .failure(.invalidData))
+                return
+            }
+            
+            task.complete(with: .success(data))
+        }
+        
+        task.request = request
+        
+        return task
+        
+    }
+}
